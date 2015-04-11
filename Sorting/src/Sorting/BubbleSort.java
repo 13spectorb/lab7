8670d8d6a55fe50b92d55dfaa2a77ce060003bf3
Ben Spector <benjamin_spector@brown.edu>
2015-04-11 15:03:17 -0400

    added bubble sort

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -7,8 +7,22 @@ public class BubbleSort {
 	 * @return					the input array, sorted least to greatest
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
-		//TODO: S1 implements bubble sort
-		
-		return null;
+		int i;
+	    int temp;
+	    boolean swapped = true;
+
+	    while(swapped){
+	    	swapped = false;
+	    	for(i = 0; i < unsortedArray.length-1; i++){
+	    		if (unsortedArray[i] > unsortedArray[i+1]){
+	    			temp = unsortedArray[i];
+	    			unsortedArray[i] = unsortedArray[i+1];
+	    			unsortedArray[i+1] = temp;
+	    			swapped = true;
+	    		}
+	    	}
+	    }
+
+	    return unsortedArray;
 	}
 }
